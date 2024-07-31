1d9c71d53f20d2a331ed77eeafa242b7f67849fc
babymilooo <Kyrylo.Yehorov@cit.khpi.edu.ua>
2024-07-31 23:14:10 +0300

    Features: get data from API and output it on display

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "./Components/Ui/avatar";
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+  const url =
+    "https://thingproxy.freeboard.io/fetch/https://live.devnimble.com/api/v1/contacts?sort=created:desc";
+  const token = process.env.REACT_APP_TOKEN;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok " + response.statusText);
+        }
+
+        const data = await response.json();
+        setContacts(data.resources);
+      } catch (error) {
+        console.error(
+          "There has been a problem with your fetch operation:",
+          error
+        );
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log(contacts);
+    // contacts?.map((contact: any) => {
+    //   console.log(contact.fields.email?.map((email: any) => email.value));
+    // }
+    // );
+  }, [contacts]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full mt-[200px] flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold">User list</h1>
+      <div>
+        {contacts?.map((contact: any) => (
+          <div key={contact.id} className="border p-2 my-2">
+            <Avatar>
+              <AvatarImage src={contact.avatar_url} alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <p>
+              {contact.fields?.["first name"]?.map((value: any) => value.value)}
+            </p>
+            <p>
+              {contact.fields?.["last name"]?.map((value: any) => value.value)}
+            </p>
+            <p>{contact.fields.email?.map((value: any) => value.value)}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
